969f4b43a9467220bfc8c4708e3bffb912eeebac
UXRXT-CYBER <128343127+UNIKBAAP@users.noreply.github.com>
2024-03-31 20:19:08 +0545

    Update run.py

--- run.py
+++ run.py
@@ -1,2 +1,2 @@
 import os
-print(" Checking for update");os.system('git pull');os.system('python3 .VIEWS/TTVIEWS.py')
+print(" Checking for update");os.system('git pull');os.system('python3 TTVIEWS/TTVIEWS.py')
