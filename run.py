901b14b7eb330720e8fe1c3da153785fdd97b00c
YunickXwd <128343127+YunickXwd@users.noreply.github.com>
2024-09-04 17:59:40 +0545

    Update run.py

--- run.py
+++ run.py
@@ -1,2 +1,8 @@
-import os
-print(" Checking for update");os.system('git pull');os.system('python3 TTVIEWS/TTVIEWS.py')
+import platform,os
+bit = platform.architecture()[0]
+print(' CHECKING FOR UPDATES');os.system('git pull')
+if bit == '32bit':
+    from data import BRZZRS32
+elif bit == '64bit':
+    from data import BRZZRS64
+else: exit(" SORRY YOUR DEVICE DOESN'T SUPPORT THIS TOOL USE 32/64 BIT PHONE");exit()
